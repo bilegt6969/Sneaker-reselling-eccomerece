b12570e46b8b40ce24a49a58733c2be15ba7a8a1
“Bilegt6969” <bilegt6969@gmail.com>
2023-09-11 03:25:20 +0800

    dadsh

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,24 +1,19 @@
-import { defineConfig, isDev, SchemaType } from 'sanity';
+import { defineConfig } from 'sanity';
 import { visionTool } from '@sanity/vision';
 import { deskTool } from 'sanity/desk';
-import { schemaTypes } from './schemas';
-
+import {schemaTypes} from './schemas';
 
 const config = defineConfig({
-  basePath:"/studio",
+  basePath: '/studio',
   name: 'default',
   title: 'salmon-cod',
   projectId: 'assvtyt4',
   dataset: 'production',
   plugins: [deskTool(), visionTool()],
   schema: {
-      types: schemaTypes,
-    },studio:{
-      components:{
-        
-      }
-
-    },
+    // Use schemaTypes directly without wrapping it in an object
+    type: schemaTypes,
+  },
 });
 
 export default config;
